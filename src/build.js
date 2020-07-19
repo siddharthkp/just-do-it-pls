b6a428cb4d101cc6b6c99b5ecc4710e119167e70
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-07-19 19:08:14 +0200

    okay geez heroku!

--- src/build.js
+++ src/build.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const nodeHtmlToImage = require('node-html-to-image');
 
-const slogans = require('./slogans.json');
+const slogans = require('../slogans.json');
 const template = fs.readFileSync('./template.html', 'utf8');
 
 Object.keys(slogans).map((date) => {
