c10a3ef228977fddb79537c2fd2cb9a6848bf085
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-07-19 14:39:47 +0200

    Everything Now - Arcade Fire

---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center">
+  <img src="https://pbs.twimg.com/profile_images/1284783237970231301/D3Ykzln2_400x400.jpg" height="200px"/>
+  <br><br>
+  <b>a collaborative "art" project</b>
+  <br><br>
+  This bot [tweets](https://twitter.com/_nikebot) a new slogan every day.
+  <br><br>
+  You are encouraged to contribute to [the slogans](https://github.com/siddharthkp/just-do-it-please)!
+</p>
+
+&nbsp;
---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const fs = require('fs');
+const twitter = require('twitter');
+const nodeHtmlToImage = require('node-html-to-image');
+
+// get today's slogan from file
+const slogans = require('./slogans.json');
+const today = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
+const todaysSlogan = slogans[today];
+const template = fs.readFileSync('./template.html', 'utf8');
+
+/** create twitter client */
+const client = new twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+(async () => {
+  const photoBuffer = await nodeHtmlToImage({
+    html: template,
+    content: [{ slogan: todaysSlogan.slogan }],
+  });
+
+  try {
+    // upload photo first
+    await client
+      .post('media/upload', { media: photoBuffer })
+      .then((screenshot) =>
+        client.post('statuses/update', {
+          media_ids: screenshot.media_id_string,
+          status: 'via ' + todaysSlogan.via,
+        })
+      )
+      .then((tweet) => {
+        // then tweet!
+        console.info('done');
+        return true;
+      });
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+})();
